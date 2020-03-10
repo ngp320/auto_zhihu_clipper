660537ef6df11405f0f13898d74582aa0f4e5d1a
ngp320 <www.610018095@qq.com>
2020-03-10 22:10:07 +0800

    完成版 Ver 1.0

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # coding=utf-8
 import logging
 import logging.config
-import os
-import time
 import traceback
 
 import yaml
@@ -13,6 +11,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 def init():
+    #调用config.yaml里的 logging配置
+    with open('config.yaml', 'r', encoding='utf-8') as f:
+        config = yaml.load(f)
+        logging.config.dictConfig(config)
     # 启用带插件的浏览器
     option = webdriver.ChromeOptions()
     option.add_argument("--user-data-dir=" + r"C:/Users/ngp/AppData/Local/Google/Chrome/User Data/")
@@ -40,32 +42,28 @@ def process(driver, url):
         xpath_content = '//a[text()="重排"]'
     elif "zhuanlan" in url:  # 点击 重新排版 （适用于专栏）
         xpath_content = '//button[text()="重新排版"]'
+    driver.implicitly_wait(10)  # 隐性等待页面 #值得注意的是 隐性等待和显性等待的最长时间取两者之中的大者
     try:
-        time.sleep(1)   #强制等待1s
-        WebDriverWait(driver, 1, 0.5).until(lambda driver: driver.find_element_by_xpath(xpath_content))
+        WebDriverWait(driver, 5, 0.5).until(lambda driver: driver.find_element_by_xpath(xpath_content))
     except TimeoutException:
         logging.error("TimeoutException 没有找到元素 : \n%s", traceback.format_exc())
         return
     except Exception:
         logging.error('Exception : \n%s', traceback.format_exc())
         return
-    print(driver.find_element_by_xpath(xpath_content).click())     # 点击 重排/重新排版
+    driver.find_element_by_xpath(xpath_content).click()     # 点击 重排/重新排版
     driver.find_element_by_tag_name('body').send_keys('`')  # 热键启动印象笔记剪藏
 
     # 点击开始剪藏
     driver.implicitly_wait(10)  # 隐性等待页面 #值得注意的是 隐性等待和显性等待的最长时间取两者之中的大者
     try:  # 尝试剪藏是否正常
         WebDriverWait(driver, 5, 0.5).until(lambda driver:driver.find_element_by_id('evernoteClipperTools'))  # 显性等待元素 （程序每隔xx秒看以下条件是否满足，如果条件成立了，则执行下一步，否则继续等待，直到超过设置的最长时间，然后抛出TimeoutException）
-        driver.switch_to_frame("evernoteClipperTools")  #切换到 嵌入iframe代码
+        driver.switch_to.frame("evernoteClipperTools")  #切换到 嵌入iframe代码
         WebDriverWait(driver, 5, 0.5).until(lambda driver:driver.find_element_by_xpath('//button[text()="保存剪藏"]'))  # 显性等待元素 （程序每隔xx秒看以下条件是否满足，如果条件成立了，则执行下一步，否则继续等待，直到超过设置的最长时间，然后抛出TimeoutException）
-        # driver.find_element_by_xpath('//button[text()="保存剪藏"]').click()
-        # ahk = AHK()
-        # ahk.mouse_move(2250, 1050, 30)
-        # ahk.click()
-        # ahk.mouse_move(500, 100, 30)
+        driver.find_element_by_xpath('//button[text()="保存剪藏"]').click()
         driver.switch_to.default_content()  # 切换到 主页代码
     except NoSuchElementException:  # 如果剪藏失败
-        logging.error('NoSuchElementException 剪藏无法开始：' + url)
+        logging.error('NoSuchElementException 剪藏无法开始：', traceback.format_exc() + url)
         return
     except Exception:
         logging.error('Exception : \n%s', traceback.format_exc())
@@ -77,9 +75,12 @@ def process(driver, url):
     try:  # 尝试剪藏是否完成
         WebDriverWait(driver, 5, 0.5).until(lambda driver: driver.find_element_by_id(
             'evernoteClipperTools'))
-        driver.switch_to_frame("evernoteClipperTools")  # 切换到 嵌入iframe代码
+        driver.switch_to.frame("evernoteClipperTools")  # 切换到 嵌入iframe代码
         WebDriverWait(driver, 60, 0.5).until(lambda driver: driver.find_element_by_xpath(
             '//a[text()="Evernote 中的视图"]'))
+        ahk = AHK()
+        ahk.mouse_move(2200, 450, 30)   # 如果剪藏完成动一下鼠标，否则不好识别
+        ahk.mouse_move(300, 100, 50)  # 如果剪藏完成动一下鼠标，否则不好识别
         driver.switch_to.default_content()  # 切换到 主页代码
     except NoSuchElementException:  # 如果剪藏失败
         logging.error('NoSuchElementException 剪藏60s无法完成 ：\n%s', traceback.format_exc() + url)
@@ -88,20 +89,12 @@ def process(driver, url):
         logging.error('Exception : \n%s', traceback.format_exc())
         return
 
-
-
-    logging.error('剪藏60s没有结束：' + url)
-
-
-    logging.info('剪藏成功' + url)
+    logging.info('剪藏成功 : ' + url)
     return 2
 
 
 
 if __name__ == "__main__":
-    with open('config.yaml', 'r', encoding='utf-8') as f:
-        config = yaml.load(f)
-        logging.config.dictConfig(config)
     driver = init()
     allUrl = getAllUrl()
     for url in allUrl:
