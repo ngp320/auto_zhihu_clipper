d4b5652b39baa456d90434f463c3a10048d10c50
ngp320 <www.610018095@qq.com>
2020-03-11 00:21:54 +0800

    添加功能: 剪藏失败，重试三次.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import logging
 import logging.config
+import time
 import traceback
 
 import yaml
@@ -34,7 +35,7 @@ def getAllUrl():
     return allUrl
 
 
-def process(driver, url):
+def process(driver, url, recursion):
     # 打开网页
     driver.get('https://' + url)
     # 点击 重排/开始重排
@@ -44,10 +45,14 @@ def process(driver, url):
         xpath_content = '//button[text()="重新排版"]'
     driver.implicitly_wait(10)  # 隐性等待页面 #值得注意的是 隐性等待和显性等待的最长时间取两者之中的大者
     try:
-        WebDriverWait(driver, 5, 0.5).until(lambda driver: driver.find_element_by_xpath(xpath_content))
+        WebDriverWait(driver, 5, 0.5).until(lambda driver: driver.find_element_by_xpath(xpath_content)) # 寻找重排
     except TimeoutException:
         logging.error("TimeoutException 没有找到元素 : \n%s", traceback.format_exc())
-        return
+        if recursion > 0:
+            logging.info("剪藏失败，倒数第%s次尝试", recursion)
+            process(driver, url, recursion - 1)
+        else:
+            return
     except Exception:
         logging.error('Exception : \n%s', traceback.format_exc())
         return
@@ -59,12 +64,17 @@ def process(driver, url):
     try:  # 尝试剪藏是否正常
         WebDriverWait(driver, 5, 0.5).until(lambda driver:driver.find_element_by_id('evernoteClipperTools'))  # 显性等待元素 （程序每隔xx秒看以下条件是否满足，如果条件成立了，则执行下一步，否则继续等待，直到超过设置的最长时间，然后抛出TimeoutException）
         driver.switch_to.frame("evernoteClipperTools")  #切换到 嵌入iframe代码
-        WebDriverWait(driver, 5, 0.5).until(lambda driver:driver.find_element_by_xpath('//button[text()="保存剪藏"]'))  # 显性等待元素 （程序每隔xx秒看以下条件是否满足，如果条件成立了，则执行下一步，否则继续等待，直到超过设置的最长时间，然后抛出TimeoutException）
+        WebDriverWait(driver, 5, 0.5).until(lambda driver:driver.find_element_by_xpath('//button[text()="保存剪藏"]'))  # 点击 保存剪藏
+        time.sleep(1)
         driver.find_element_by_xpath('//button[text()="保存剪藏"]').click()
         driver.switch_to.default_content()  # 切换到 主页代码
     except NoSuchElementException:  # 如果剪藏失败
         logging.error('NoSuchElementException 剪藏无法开始：', traceback.format_exc() + url)
-        return
+        if recursion > 0:
+            logging.info("剪藏失败，倒数第%s次尝试", recursion)
+            process(driver, url, recursion - 1)
+        else:
+            return
     except Exception:
         logging.error('Exception : \n%s', traceback.format_exc())
         return
@@ -73,17 +83,16 @@ def process(driver, url):
 
     # 检测到 剪藏完成则开始剪藏下一个页面
     try:  # 尝试剪藏是否完成
-        WebDriverWait(driver, 5, 0.5).until(lambda driver: driver.find_element_by_id(
-            'evernoteClipperTools'))
+        WebDriverWait(driver, 5, 0.5).until(lambda driver: driver.find_element_by_id('evernoteClipperTools'))
         driver.switch_to.frame("evernoteClipperTools")  # 切换到 嵌入iframe代码
         WebDriverWait(driver, 60, 0.5).until(lambda driver: driver.find_element_by_xpath(
             '//a[text()="Evernote 中的视图"]'))
-        ahk = AHK()
-        ahk.mouse_move(2200, 450, 30)   # 如果剪藏完成动一下鼠标，否则不好识别
-        ahk.mouse_move(300, 100, 50)  # 如果剪藏完成动一下鼠标，否则不好识别
+        # ahk = AHK()
+        # ahk.mouse_move(2200, 450, 30)   # 如果剪藏完成动一下鼠标，测试的时候方便肉眼判断
+        # ahk.mouse_move(300, 100, 50)    # 如果剪藏完成动一下鼠标，测试的时候方便肉眼判断
         driver.switch_to.default_content()  # 切换到 主页代码
     except NoSuchElementException:  # 如果剪藏失败
-        logging.error('NoSuchElementException 剪藏60s无法完成 ：\n%s', traceback.format_exc() + url)
+        logging.error('NoSuchElementException 剪藏60s无法完成 ：\n%s', traceback.format_exc() + '\n' + url)
         return
     except Exception:
         logging.error('Exception : \n%s', traceback.format_exc())
@@ -98,5 +107,5 @@ if __name__ == "__main__":
     driver = init()
     allUrl = getAllUrl()
     for url in allUrl:
-        process(driver, url)
+        process(driver, url, 3) #找不到排版 最多刷新三次
     driver.close()
--- readme.md
+++ readme.md
@@ -9,13 +9,11 @@ pycharm 运行的时候关闭 全局vpn（例如proxifier）
 # 使用说明
 1 点开知乎 → 点头像 翻到状态 
 
-2 使用ahk的自动PgDn，翻到没有剪藏的那一页（大概估计一下天数即可，
-
-印象笔记搜索 "- 知乎" 能看到最近剪藏的网页），
+2 使用ahk的自动PgDn，翻到没有剪藏的那一页（大概估计一下天数即可，印象笔记搜索 "- 知乎" 能看到最近剪藏的网页），
 
 F12 → copy outHtml → 用 "记事本" 或 "notepad++" 粘贴到 link.txt
 
-3 用python 正则提取所有 question和zhuanlan 的链接link以及 question名
+3 用python(pre.py) 正则提取所有 question和zhuanlan 的链接link以及 question名, 到newLink
 
 4 根据印象笔记，删除多余的链接
 
